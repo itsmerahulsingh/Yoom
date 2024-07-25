a84d69043a9ca841ba65b722e9c8f0e010671911
“itsmerahulsingh” <hotrahul5275@gmail.com>
2024-07-25 14:30:48 +0530

    first commit

---
+++ app/(root)/(home)/previous/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CallList from "@/components/CallList";
+
+const Previous = () => {
+  return (
+    <section className='flex size-full flex-col gap-10 text-white'>
+    <h1 className='text-3xl font-bold'>
+    <CallList type="ended" />
+    </h1>
+  </section>
+  )
+}
+
+export default Previous
---
+++ app/(root)/(home)/recordings/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CallList from '@/components/CallList';
+
+const Recordings = () => {
+  return (
+    <section className='flex size-full flex-col gap-10 text-white'>
+      <h1 className='text-3xl font-bold'>
+      <CallList type="recordings" />
+      </h1>
+      
+    </section>
+  )
+}
+
+export default Recordings
